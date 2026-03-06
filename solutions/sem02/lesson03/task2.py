e30c4cf8bd5576849e94151e55d6110cf05f9425
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2026-03-06 23:31:57 +0300

    Б12-514 Рябоконь Илья: выполненые задания 1,2,3 из lesson 3

--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -9,11 +9,29 @@ def convert_from_sphere(
     distances: np.ndarray,
     azimuth: np.ndarray,
     inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    
+    if not (distances.shape == azimuth.shape == inclination.shape):
+        raise ShapeMismatchError
+    
+    abcissa = distances * np.sin(inclination) * np.cos(azimuth)
+    ordinates = distances * np.sin(inclination) * np.sin(azimuth)
+    applicates = distances * np.cos(inclination)
+
+    return (abcissa, ordinates, applicates)
 
 
 def convert_to_sphere(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    
+    if not (abscissa.shape == ordinates.shape == applicates.shape):
+        raise ShapeMismatchError
+    
+    distances = np.sqrt(abscissa ** 2 + ordinates ** 2 + applicates ** 2)
+    azimuth = np.arctan2(ordinates, abscissa)
+    inclination = np.arctan2(applicates, np.sqrt(abscissa ** 2 + ordinates ** 2))
+
+    return (distances, azimuth, inclination)
