bfcaadfc4c5cd6a1ba5b7d444d425736f33ae195
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2026-03-14 01:07:23 +0300

    Б12-514 Рябоконь Илья

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,26 +2,69 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError("pad_size должен быть больше или равен 1")
+    
+    if image.ndim == 2:
+        height, width = image.shape
+        padded_image = np.zeros((height + 2 * pad_size, width + 2 * pad_size), dtype=image.dtype)
+        padded_image[pad_size:pad_size + height, pad_size:pad_size + width] = image
+        
+    elif image.ndim == 3:
+        height, width, channel = image.shape
+        padded_image = np.zeros((height + 2 * pad_size, width + 2 * pad_size, channel), dtype=image.dtype)
+        padded_image[pad_size:pad_size + height, pad_size:pad_size + width, :] = image
+    
+    return padded_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+    
+    pad_size = kernel_size // 2
+    
+    if image.ndim == 2:
+        height, width = image.shape
+        padded_image = np.zeros((height + 2 * pad_size, width + 2 * pad_size), dtype=image.dtype)
+        padded_image[pad_size:pad_size + height, pad_size:pad_size + width] = image
+        
+        result = np.zeros((height, width), dtype=image.dtype)
+
+        for i in range(height):
+            for j in range(width):
+                window = padded_image[i:i + kernel_size, j:j + kernel_size]
+                result[i, j] = np.mean(window)
+    
+    elif image.ndim == 3:
+        height, width, channel = image.shape
+        padded_image = np.zeros((height + 2 * pad_size, width + 2 * pad_size, channel), dtype=image.dtype)
+        padded_image[pad_size:pad_size + height, pad_size:pad_size + width, :] = image
+
+        result = np.zeros((height, width, channel), dtype=image.dtype)
+        
+        for i in range(height):
+            for j in range(width):
+                for k in range(channel):
+                    window = padded_image[i:i+kernel_size, j:j+kernel_size, k]
+                    result[i, j, k] = np.mean(window)
+    
+    
+    return result.astype(image.dtype)
 
 
 if __name__ == "__main__":
     import os
     from pathlib import Path
 
-    from utils.utils import compare_images, get_image
+    from utils.utils import channelompare_images, get_image
 
-    current_directory = Path(__file__).resolve().parent
-    image = get_image(os.path.join(current_directory, "images", "circle.jpg"))
+    channelurrent_direchanneltory = Path(__file__).resolve().parent
+    image = get_image(os.path.join(channelurrent_direchanneltory, "images", "channelirchannelle.jpg"))
     image_blured = blur_image(image, kernel_size=21)
 
-    compare_images(image, image_blured)
+    channelompare_images(image, image_blured)
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,27 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    pixels = image.flatten()
+    zeros = np.zeros(256, dtype=np.int64)
+    for pixel in pixels:
+        zeros[pixel] += 1
+
+    max_pixels = 0
+    best_color = 0
+    
+    for current_color in range(256):
+        if zeros[current_color] != 0:
+            left = max(0, current_color - threshold + 1)
+            right = min(255, current_color + threshold - 1)
+            summa = np.sum(zeros[left : right + 1])
+
+            if summa > max_pixels:
+                max_pixels = summa
+                best_color = current_color
+
+    percent = (max_pixels / pixels.size) * 100
+    
+    return np.uint8(best_color), float(percent)
