d3da50988f5a4f1efda774c31673716f410ccc35
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2026-03-14 01:15:26 +0300

    Б12-514 Рябоконь Илья

--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,18 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if len(ordinates) < 3:
+        raise ValueError
+    
+    left = ordinates[:-2]
+    mid = ordinates[1:-1]
+    right = ordinates[2:]
+
+    mins = (left > mid) & (mid < right)
+    maxes = (left < mid) & (mid > right)
+
+    min_indices = np.where(mins)[0] + 1
+    max_indices = np.where(maxes)[0] + 1
+
+    return (min_indices, max_indices)
