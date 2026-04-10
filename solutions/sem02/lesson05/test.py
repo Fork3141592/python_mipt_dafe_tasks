ca7e76d33d3db296b87593915255ece10dbe6d70
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2026-04-10 23:43:55 +0300

    Б12-514 Рябоконь Илья

---
+++ solutions/sem02/lesson05/test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = [[True, True],
+     [False, True]]
+
+s = np.where(a)
+s_s = np.where(a)[0][0]
+
+print(a)
+print(s)
+print(s_s)
+
+
+mins = [True, False, True, False]
+maxes = [False, True, False, True]
+
+min_indices = np.where(mins)[0]
+s_min_indices = np.where(mins)
+print(min_indices)
+print(s_min_indices)
